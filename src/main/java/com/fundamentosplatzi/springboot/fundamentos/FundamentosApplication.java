1d2360c18c4f17e1c0ed0ba4fee933c9c90edba7
SebasPertuz <sebastianpertuzg@hotmail.com>
2023-03-02 08:13:34 -0500

    Día en que me perdí mucho con tantas dependencias y beans

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,5 +1,6 @@
 package com.fundamentosplatzi.springboot.fundamentos;
 
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -10,8 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FundamentosApplication implements CommandLineRunner{
 
 	private ComponentDependency componentDependency;
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency){
+	private MyBean myBean;
+
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean){
 		this.componentDependency = componentDependency;
+		this.myBean = myBean;
 	}
 
 	public static void main(String[] args) {
@@ -21,5 +25,6 @@ public class FundamentosApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		componentDependency.saludar();
+		myBean.print();
 	}
 }
